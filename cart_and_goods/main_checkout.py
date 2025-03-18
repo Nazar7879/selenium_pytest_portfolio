777e9d296484fc7bbf9684e49eb0411095bf5268
Nazar7879 <nextlevel.skills.0001@gmail.com>
2025-03-18 14:26:23 -0230

    new version

--- cart_and_goods/main_checkout.py
+++ cart_and_goods/main_checkout.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import  time
 
 
 class ProductCheckout:
